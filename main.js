f5b997ad07a659b0f6caf9829a5d2282d28e648c
Ajeet <ajeet.tiwari@helloworld.com.au>
2018-06-04 09:58:37 +1000

    code skimming

--- main.js
+++ main.js
@@ -2,19 +2,13 @@ const express = require("express");
 
 const port=process.env.PORT||5050;
 
-const bodyParser = require("body-parser");
-
 const routes = require("./src/script/route.js");
 
 const app = express();
 
 app.use('/',express.static(__dirname + '/src/view'));
 
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/run',routes);
+app.use('/convert',routes);
 
 const server = app.listen(port, function () {
 
